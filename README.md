d38d90f87bdf2c3feef65c9a410cee3f9c2c9cc8
Sunf3r <sunf3r@proton.me>
2023-03-24 21:30:21 -0300

    add try/catch to gh requests

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 - Olá caro visitante
 - Essa bosta cringe é feita com o intúito de ser um script multiuso
-- que você pode compilar com `deno compile -A Main.ts`, passar pra um pen drive
+- que você pode compilar com `deno compile -A Updater.ts`, passar pra um pen
+  drive
 - e rodar em qualquer computador com Windows 10/11. Jamais use-o
 - para prejudicar alguém, mas vc pode... talvez...
 - usar para roubar a senha do Wi-Fi da sua escola
 - e impressionar os seus amigos com as suas
-- incríveis habilidades como um ✨ Script Kiddie ✨
+- incríveis habilidades de ✨ Script Kiddie ✨
--- Updater.ts
+++ Updater.ts
@@ -3,39 +3,52 @@
  * do commit mais recente no GitHub
  */
 
-// Baixando a lista de arquivos
-const request = await fetch(
-	`https://api.github.com/repos/Sunf3r/WiFi_Cloner/contents`,
-);
-// Filtrando o nome dos arquivos
-const files: string[] = (await request.json())
-	.map((file: { name: string }) => file.name);
+const showLogs = !Deno.args.includes('-q');
 
-for (const f of files) {
-	if (!f.endsWith('.ts')) continue; // Apenas arquivos .TS
+try {
+	// Baixando a lista de arquivos
+	const request = await fetch(
+		`https://api.github.com/repos/Sunf3r/WiFi_Cloner/contents`,
+	);
 
-	let req;
-	try {
-		req = await fetch( // Lendo o arquivo
-			`https://raw.githubusercontent.com/Sunf3r/WiFi_Cloner/master/${f}`,
-		);
-		console.log(
-			`%c[UPDATER] %c- ${f} fetched!`,
-			'color: cyan',
-			'color: green',
-		);
+	// Filtrando o nome dos arquivos
+	const files: string[] = (await request.json())
+		.map((file: { name: string }) => file.name);
 
-		// Escrevendo o arquivo
-		await Deno.writeTextFile(`./${f}`, await req.text());
-	} catch (e) {
-		console.log(
-			`%c[UPDATER] %c- Error when fetching %c${f}\n%c${e}`,
-			'color: cyan',
-			'color: red',
-			'color: white; background-color: red',
-			'color: red',
-		);
+	for (const f of files) {
+		if (!f.endsWith('.ts')) continue; // Apenas arquivos .TS
+		if (Deno.args.includes('--dev') && f === 'Updater.ts') continue;
+		// Não atualizar o updater enquanto estiver em dev time
+
+		let req;
+		try {
+			req = await fetch( // Lendo o arquivo
+				`https://raw.githubusercontent.com/Sunf3r/WiFi_Cloner/master/${f}`,
+			);
+			showLogs && console.log(
+				`%c[UPDATER] %c- ${f} fetched!`,
+				'color: cyan',
+				'color: green',
+			);
+
+			// Escrevendo o arquivo
+			await Deno.writeTextFile(`./${f}`, await req.text());
+		} catch (e) {
+			showLogs && console.log(
+				`%c[UPDATER] %c- Error when updating %c${f}\n%c${e}`,
+				'color: cyan',
+				'color: red',
+				'color: white; background-color: red',
+				'color: red',
+			);
+		}
 	}
+} catch (e) {
+	showLogs && console.log(
+		`%c[UPDATER] %c- Error when updating files\n${e}`,
+		'color: cyan',
+		'color: red',
+	);
+} finally {
+	await import('./Main.ts');
 }
-
-await import('./Main.ts');
